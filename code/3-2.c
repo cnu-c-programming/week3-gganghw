bb00842e41afd032a5516d11bae81d271ba9924f
gganghw <sunflower110714@gmail.com>
2026-03-22 03:50:53 +0000

    to 3-5

--- code/3-2.c
+++ code/3-2.c
@@ -1,6 +1,23 @@
 #include <stdio.h>
+int global_var = 0;
 
-int main() {
+void test_scope(){
+    static int static_var = 0;
+    int local_var = 0;
+
+    global_var++;
+    static_var++;
+    local_var++;
+    printf("global_var: %d\n", global_var);
+    printf("static_var: %d\n", static_var);
+    printf("local_var: %d\n", local_var);
+}
+int main() {//      g s l
+    test_scope();// 1 1 1
+    printf("\n");
+    test_scope();// 2 2 1
+    printf("\n");
+    test_scope();// 3 3 1
 
     return 0;
 }
--- code/3-5.c
+++ code/3-5.c
@@ -1,7 +1,37 @@
 #include <stdio.h>
 #include <stdarg.h>
-
+void my_sum(char type, int count, ...){
+    va_list ap;
+    va_start(ap, count);
+    switch(type){
+        case 'S':
+        for(int i = 0; i < count; i++){
+            printf("%s ",va_arg(ap, char *));
+        }
+        va_end(ap);
+        printf("\n");
+        break;
+        case 'C':
+        for(int i = 0; i < count; i++){
+            printf("%c", va_arg(ap, int));
+        }
+        va_end(ap);
+        printf("\n");
+        break;
+        case 'D':
+        int total = 0;
+        for(int i = 0; i < count; i++){
+            total += va_arg(ap, int);
+        }
+        va_end(ap);
+        printf("%d\n", total);
+        break;
+    }
+}
 int main() {
-
+    my_sum('S', 2, "Hello", "World");
+    my_sum('C', 3, 'C', 'N', 'U');
+    my_sum('D', 4, 10, 20, 30, 40);
+    
     return 0;
 }
